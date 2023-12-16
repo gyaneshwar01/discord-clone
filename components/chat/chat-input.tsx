d716ae7be2a4ae290ceb6e6be4ac9fdcd7168db9
Gyaneshwar Sah <gyaneshwar585@gmail.com>
2023-12-16 11:45:18 +0545

    Chat Messages

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -5,7 +5,7 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Plus, Smile } from "lucide-react";
+import { Plus } from "lucide-react";
 import axios from "axios";
 import qs from "query-string";
 import { useModal } from "@/hooks/use-modal-store";
---
+++ components/providers/query-provider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { useState } from "react";
+
+export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
